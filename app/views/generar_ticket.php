df765d63c34aed5574bf5baab301809b34d8d739
Diego Alvarado <dialvaradov@alumno.unsm.edu.pe>
2023-04-02 12:59:22 -0500

    pdfIndividual

--- app/views/generar_ticket.php
+++ app/views/generar_ticket.php
@@ -1,4 +1,5 @@
 <?php
+
 $medidaTicket = 180;
 
 ?>
@@ -21,46 +22,21 @@ $medidaTicket = 180;
             margin: 2px;
         }
 
-        td,
-        th,
-        tr,
-        table {
-            border-top: 1px solid black;
-            border-collapse: collapse;
-            margin: 0 auto;
-        }
-
-        td.precio {
-            text-align: right;
-            font-size: 11px;
-        }
-
-        td.cantidad {
-            font-size: 11px;
-        }
-
-        td.producto {
-            text-align: center;
-        }
-
-        th {
-            text-align: center;
-        }
-
-
+       
         .centrado {
             text-align: center;
             align-content: center;
         }
-
         .ticket {
             width: <?php echo $medidaTicket ?>px;
             max-width: <?php echo $medidaTicket ?>px;
         }
 
-        img {
-            max-width: inherit;
-            width: inherit;
+        .barras{
+            width:50%;
+        }
+        .qr{
+            
         }
 
         * {
@@ -75,76 +51,36 @@ $medidaTicket = 180;
 
         body {
             text-align: center;
+            
         }
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="ticket centrado">
-        <h1>NOMBRE DEL NEGOCIO</h1>
-        <h2>Ticket de venta #12</h2>
-        <h2>2020-02-05 00:12:22</h2>
-        <?php
-        # Recuerda que este arreglo puede venir de cualquier lugar; aquí lo defino manualmente para simplificar
-        # Puedes obtenerlo de una base de datos, por ejemplo: https://parzibyte.me/blog/2019/07/17/php-bases-de-datos-ejemplos-tutoriales-conexion/
+        
+        <h2><?php echo $inventario->nombre ?></h2>
+        <h2><?php echo $inventario->fecha ?></h2>
+        <div class="row">
+            <div class="col-3">
+            <?php $src1="http://localhost:8012/backend_ticket/inventarioBarra/".$codigo;
+            echo '<img class="barras" src="'.$src1.'" alt="">';
+            ?> 
+            <h4><?php echo $codigo ?></h4>
+            </div>
 
-        $productos = [
-            [
-                "cantidad" => 31,
-                "descripcion" => "Cheetos verdes 80 g",
-                "precio" => 123,
-            ],
-            [
-                "cantidad" => 12,
-                "descripcion" => "Teclado HyperX",
-                "precio" => 1233,
-            ],
-            [
-                "cantidad" => 12,
-                "descripcion" => "Mouse Logitech ASD123",
-                "precio" => 841,
-            ],
-            [
-                "cantidad" => 15,
-                "descripcion" => "Monitor Samsung 123",
-                "precio" => 3546,
-            ],
-        ];
-        ?>
-
-        <table>
-            <thead>
-                <tr class="centrado">
-                    <th class="cantidad">CANT</th>
-                    <th class="producto">PRODUCTO</th>
-                    <th class="precio">$$</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $total = 0;
-                foreach ($productos as $producto) {
-                    $total += $producto["cantidad"] * $producto["precio"];
-                ?>
-                    <tr>
-                        <td class="cantidad"><?php echo number_format($producto["cantidad"], 2) ?></td>
-                        <td class="producto"><?php echo $producto["descripcion"] ?></td>
-                        <td class="precio">$<?php echo number_format($producto["precio"], 2) ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-            <tr>
-                <td class="cantidad"></td>
-                <td class="producto">
-                    <strong>TOTAL</strong>
-                </td>
-                <td class="precio">
-                    $<?php echo number_format($total, 2) ?>
-                </td>
-            </tr>
-        </table>
-        <p class="centrado">¡GRACIAS POR SU COMPRA!
-            <br>parzibyte.me</p>
+            <div class="col-4">
+            <?php $src2="http://localhost:8012/backend_ticket/inventarioQR/".$codigo;
+            echo '<img class="qr" src="'.$src2.'" alt="">';
+            ?> 
+            </div>
+        </div>
+       
+        
+        
+        <!-- <p class="centrado">¡GRACIAS POR SU COMPRA!
+            <br>parzibyte.me</p> -->
     </div>
 </body>
 
